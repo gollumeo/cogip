60b334b914487b22351bc2ac871245562937a15a
MathildeCornelis <mathilde.cornelis26@gmail.com>
2023-02-06 09:05:45 +0100

    page Home React

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,15 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Nav from './assets/component/Nav'
-import Manage from './assets/component/Manage'
-import Last_invoices from './assets/component/Last_invoices'
-import Last_contacts from './assets/component/Last_contacts'
-
+import App from './App'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Nav />
-    <Manage />
-    <Last_invoices />
-    <Last_contacts />
+    <App />
   </React.StrictMode>,
 )
