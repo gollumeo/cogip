6c055439b8d7d7a9f9159405d84f8fb3e31fd860
MathildeCornelis <mathilde.cornelis26@gmail.com>
2023-02-06 10:43:48 +0100

    add white shape at manage component

--- front/src/assets/component/Manage.jsx
+++ front/src/assets/component/Manage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Homme_1 from '/src/assets/img/image_homme_1.png';
 import "/src/assets/scss/Manage.css";
+import white_shape from "/src/assets/img/Rectangle 10.png"
 
 
 const Manage = () => {
@@ -8,6 +9,7 @@ const Manage = () => {
         <section className='sect_manage'>
             <h2 className='manage'>MANAGE YOUR CUSTOMERS AND INVOICES EASLY</h2>
             <img src={Homme_1} alt="Homme" className='homme_1'/>
+            <img src={white_shape} alt="white shape" className='white_shape' />
         </section>
     );
 };
