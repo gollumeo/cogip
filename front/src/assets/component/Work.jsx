0543f55fb15cceb4e84a763ab03c375364aec108
MathildeCornelis <mathilde.cornelis26@gmail.com>
2023-02-06 16:57:06 +0100

    footer page home

--- front/src/assets/component/Work.jsx
+++ front/src/assets/component/Work.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import "/src/assets/scss/Work.css"
-import comments from "/public/img/comments.png";
-import yellow_rectangle from "/public/img/Yellow_ectangle.png"
+import "/src/assets/scss/Work.scss";
+import yellow_rectangle_comments from "/img/yellow_rectangle_comments.png"
 
 const Work = () => {
     return (
         <section className='work_section'>
             <h2>WORK BETTER IN YOUR COMPANY</h2>
-                <div className='yellow_comments'>
-                    <img src={yellow_rectangle} alt="yellow rectangle" className='yellow_rectangle'/>
-                    <img src={comments} alt="comments" className='comments'/>
-                </div>
+                <img src={yellow_rectangle_comments} alt="yellow rectangle comments" className='yellow_rectangle_comments'/>
             </section>
     );
 };
