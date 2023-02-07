69141f529e08cb526144b20a009cdc86abf7f9ed
MathildeCornelis <mathilde.cornelis26@gmail.com>
2023-02-07 16:54:29 +0100

    burger menu + desktop menu on Nav.jsx + scss

--- front/src/assets/component/Nav.jsx
+++ front/src/assets/component/Nav.jsx
@@ -1,29 +1,49 @@
 import React from 'react';
 import "/src/assets/scss/Nav.css";
 import BurgerMenu from '/public/img/burger_menu_icon.png';
+import { useState } from 'react';
 
-const Nav = () => {
-  return (
-    <header>
-      <h1>COGIP</h1>
-      <nav className='nav_pages'>
+function Nav(){
+
+  const [ isOpen, setIsOpen ] = useState(false);
+
+
+  // const showBurgerMenu = () => {
+  //   setIsOpen(!isOpen)
+  //   console.log(isOpen);
+  // };
+
+
+const displayValue = isOpen ? 'flex' : 'none';
+
+return (
+  <header>
+    <h1>COGIP</h1>
+    <nav className="all">
+      <button className="burger" onClick={() => setIsOpen(!isOpen)}>
+        <img src={BurgerMenu} alt="BurgerMenu" className='burger_menu'/>
+      </button>
+      <div className="nav_pages" style={{ display: displayValue, ...(window.matchMedia("(min-width: 1280px)").matches && {display: 'flex'}) }}>
         <ul className='list_pages'>
           <li className='page'><a href="#">Home</a></li>
           <li className='page'><a href="#">Invoices</a></li>
           <li className='page'><a href="#">Companies</a></li>
           <li className='page'><a href="#">Contacts</a></li>
         </ul>
-      </nav>
-      <nav className='nav_log'>
+      </div>
+      <div className="nav_log" style={{ display: displayValue, ...(window.matchMedia("(min-width: 1280px)").matches && {display: 'flex'}) }}>
         <ul className='list_log'>
           <li className='sign_up'><a href="#">Sign up</a></li>
           <li className='login'><a href="#">Login</a></li>
         </ul>
-      </nav>
-      <button><img src={BurgerMenu} alt="BurgerMenu" className='burger_menu'/></button>
-    </header>
-  );
+      </div>
+    </nav>
+  </header>
+);
+
 };
+
+
 
 export default Nav;
 
