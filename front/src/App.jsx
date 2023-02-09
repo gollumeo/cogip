b5c6a82d8a15f93d13991936ca1ff80290d43fdf
Benjamin Porsont <porsont.benjamin@gmail.com>
2023-02-09 13:58:35 +0100

    modif

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './assets/pages/Home';
-import Invoices from './assets/pages/Invoices';
-import Companies from './assets/pages/Companies';
-import Contacts from './assets/pages/Contacts';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./assets/pages/Home";
+import Invoices from "./assets/pages/Invoices";
+import Companies from "./assets/pages/Companies";
+import Contacts from "./assets/pages/Contacts";
+import Show_invoices from "./assets/pages/Show_invoices";
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/invoices' element={<Invoices />} />
-                <Route path='/companies' element={<Companies />} />
-                <Route path='/contacts' element={<Contacts />} />
-                <Route path='*' element={<Home />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/invoices" element={<Invoices />} />
+        <Route path="/companies" element={<Companies />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/show_invoices" element={<Show_invoices />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
