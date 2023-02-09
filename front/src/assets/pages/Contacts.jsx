84a3bdb11d16261991d76df390426ec4558d2b9d
MathildeCornelis <mathilde.cornelis26@gmail.com>
2023-02-09 16:58:19 +0100

    add pages Invoices, Contacts and companies

--- front/src/assets/pages/Contacts.jsx
+++ front/src/assets/pages/Contacts.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import All_contacts from '../component/All_contacts';
+import Nav from '../component/Nav'
+import Footer from '../component/Footer'
 
 const Contacts = () => {
     return (
         <div>
-            <h1>Contacts</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi sunt et illo accusamus, consectetur minima distinctio reiciendis accusantium aut harum quisquam excepturi dolorem perferendis labore! Modi ut maxime incidunt fugiat magnam perspiciatis repudiandae dolore suscipit soluta numquam aliquid, autem accusamus nobis beatae natus atque quaerat cupiditate, tempora labore necessitatibus qui quae! Natus beatae ipsam saepe consequuntur nihil rerum cum eveniet, dolores fugit recusandae corporis explicabo. Quos nobis impedit blanditiis voluptate quae dolorum sapiente, illum dolorem atque, dignissimos facere eaque laborum. Dolorem temporibus possimus voluptate repellat id ipsa, iste consequuntur voluptatibus maiores eaque dignissimos animi, voluptatum nemo dicta illum adipisci voluptas.</p>
-            <br />
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil minus voluptate consectetur, iste omnis perferendis temporibus illum provident ipsum praesentium architecto hic quis aliquam asperiores, maxime tempore explicabo ipsa, odio error saepe. Eius dolorum molestiae laboriosam amet aspernatur ipsa illum! Sapiente voluptate illo, beatae officiis, odio voluptatum quae minima accusamus iure perferendis facere ratione cupiditate magnam, repellendus suscipit? Et consequuntur commodi, necessitatibus minus praesentium earum architecto vel reprehenderit impedit dolorum recusandae similique distinctio totam. Iusto excepturi reiciendis exercitationem aspernatur aliquam unde accusamus dolore, incidunt illo nesciunt, inventore libero veniam omnis ipsam saepe dolorum consequatur totam ipsum qui sint nisi voluptates!</p>
-
+            <Nav />
+            <All_contacts />
+            <Footer />
         </div>
     );
 };
