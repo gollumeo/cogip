a31caa496a3ef71961663760af4883f36cd4aee9
MathildeCornelis <mathilde.cornelis26@gmail.com>
2023-02-09 11:46:33 +0100

    dashboard

--- front/src/assets/pages/Dashboard.jsx
+++ front/src/assets/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Dashboard = () => {
     return (
         <div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis earum delectus commodi dolores. Deleniti, asperiores, explicabo at quaerat illo labore ab autem nostrum saepe nisi cumque, neque repellendus maxime quisquam?
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ipsum libero quisquam repellendus dolor soluta natus sit doloribus aut delectus, excepturi recusandae, cupiditate veniam odit at ut quod, aperiam voluptate.
         </div>
     );
