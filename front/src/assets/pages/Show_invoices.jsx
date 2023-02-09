8604fc68cf9f0286c983590071cec27574d81171
Benjamin Porsont <porsont.benjamin@gmail.com>
2023-02-09 13:58:35 +0100

    modif

---
+++ front/src/assets/pages/Show_invoices.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../component/Footer";
+import Nav2 from "../component/Nav2";
+
+const Show_invoices = () => {
+  return (
+    <div>
+      <Nav2 />
+      <Footer />
+    </div>
+  );
+};
+
+export default Show_invoices;
