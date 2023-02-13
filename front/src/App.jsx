3c74995e3755b3bb6c48ea7583f16cc2af487851
Benjamin Porsont <porsont.benjamin@gmail.com>
2023-02-13 12:05:51 +0100

    modif

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -5,6 +5,7 @@ import Invoices from "./assets/pages/Invoices";
 import Companies from "./assets/pages/Companies";
 import Contacts from "./assets/pages/Contacts";
 import Show_invoices from "./assets/pages/Show_invoices";
+import Show_contact from "./assets/pages/Show_contact";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/companies" element={<Companies />} />
         <Route path="/contacts" element={<Contacts />} />
         <Route path="/show_invoices" element={<Show_invoices />} />
+        <Route path="/show_contact" element={<Show_contact />} />
         <Route path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>
