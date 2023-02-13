83de21358295535c1e9d9a5a1aeb4cb5a083eeb9
Milo Bonnet <milovtt@gmail.com>
2023-02-13 12:29:58 +0100

    Login & Register & Logout

---
+++ front/logout.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+// Supprimer les variables de session
+session_unset();
+
+// Détruire la session
+session_destroy();
+
+// Rediriger l'utilisateur vers la page de connexion
+header('Location: login.php');
+exit;
+?>
