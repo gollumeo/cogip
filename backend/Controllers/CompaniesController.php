77f4e972683d0eacf81b58312ea960613ecbd8ab
Pierre M <p.mauriello54@gmail.com>
2023-02-13 13:56:18 +0100

    api format

--- backend/Controllers/CompaniesController.php
+++ backend/Controllers/CompaniesController.php
@@ -6,75 +6,85 @@ use App\Models\Companies;
 use App\Repositories\CompaniesRepository;
 use App\Services\CompaniesService;
 
-
 class CompaniesController
 {
     private $companiesService;
 
     public function __construct()
     {
-        $this->companiesRepository = new CompaniesRepository();
-        $this->companiesService = new CompaniesService();
+        $this->companiesService = new CompaniesService(new CompaniesRepository());
     }
 
     public function createCompanies()
     {
         $companiesData = new Companies();
 
-        $companiesData->setName($_POST['name']);
-        $companiesData->setTypeid($_POST['type_id']);
-        $companiesData->setCountry($_POST['country']);
-        $companiesData->setTva($_POST['tva']);
+        $companiesData->setName(filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING));
+        $companiesData->setTypeid(filter_input(INPUT_POST, 'type_id', FILTER_VALIDATE_INT));
+        $companiesData->setCountry(filter_input(INPUT_POST, 'country', FILTER_SANITIZE_STRING));
+        $companiesData->setTva(filter_input(INPUT_POST, 'tva', FILTER_SANITIZE_STRING));
         $companiesData->setCreatedAt(date('Y-m-d H:i:s'));
         $companiesData->setUpdatedAt(date('Y-m-d H:i:s'));
 
-        $this->companiesService->createCompanies($companiesData);
-
-        echo "Company successfully created!";
+        if ($this->companiesService->createCompanies($companiesData)) {
+            echo "Company successfully created!";
+        } else {
+            echo "Company creation failed!";
+        }
     }
 
     public function updateCompanies()
     {
         $companiesData = new Companies();
 
-        $companiesData->setName($_POST['name']);
-        $companiesData->setTypeid($_POST['type_id']);
-        $companiesData->setCountry($_POST['country']);
-        $companiesData->setTva($_POST['tva']);
+        $companiesData->setName(filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING));
+        $companiesData->setTypeid(filter_input(INPUT_POST, 'type_id', FILTER_VALIDATE_INT));
+        $companiesData->setCountry(filter_input(INPUT_POST, 'country', FILTER_SANITIZE_STRING));
+        $companiesData->setTva(filter_input(INPUT_POST, 'tva', FILTER_SANITIZE_STRING));
         $companiesData->setCreatedAt(date('Y-m-d H:i:s'));
         $companiesData->setUpdatedAt(date('Y-m-d H:i:s'));
 
-        $this->companiesService->createCompanies($companiesData);
-
-        echo "Company successfully updated!";
+        if ($this->companiesService->updateCompanies($companiesData)) {
+            echo "Company successfully updated!";
+        } else {
+            echo "Company update failed!";
+        }
     }
 
     public function deleteCompanies()
     {
-        $companiesData = new Companies();
+        $id = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
 
-        $companiesData->setId($_POST['id']);
-
-        $this->companiesService->deleteCompanies($companiesData);
-
-        echo "Company successfully deleted!";
+        if ($this->companiesService->deleteCompanies($id)) {
+            echo "Company successfully deleted!";
+        } else {
+            echo "Company deletion failed!";
+        }
     }
 
     public function getAllCompanies()
     {
         $allCompanies = $this->companiesService->getAllCompanies();
         header('Content-Type: application/json');
-        echo $allCompanies;
+        echo json_encode($allCompanies, JSON_PRETTY_PRINT);
     }
 
     public function getCompaniesById()
     {
         $companiesData = new Companies();
 
-        $companiesData->setId($_GET['id']);
+        $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+        if (!$id) {
+            http_response_code(400);
+            echo json_encode(['error' => 'Invalid ID']);
+            return;
+        }
+        $companiesData->setId($id);
 
-        $companiesByid = $this->companiesService->getCompaniesByid($companiesData);
+        $companiesById = $this->companiesService->getCompaniesById($companiesData);
+
         header('Content-Type: application/json');
-        echo $companiesByid;
+        echo json_encode($companiesById);
     }
+
 }
