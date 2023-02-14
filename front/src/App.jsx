b91dcd8279946f3eb807a1612c37379966bf1b6c
Benjamin Porsont <porsont.benjamin@gmail.com>
2023-02-14 16:59:25 +0100

    modif

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -5,14 +5,9 @@ import Invoices from "./assets/pages/Invoices";
 import Companies from "./assets/pages/Companies";
 import Contacts from "./assets/pages/Contacts";
 import Dashboard from "./assets/pages/Dashboard";
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./assets/pages/Home";
-import Invoices from "./assets/pages/Invoices";
-import Companies from "./assets/pages/Companies";
-import Contacts from "./assets/pages/Contacts";
 import Show_invoices from "./assets/pages/Show_invoices";
 import Show_contact from "./assets/pages/Show_contact";
+import Dashboard_new_invoice from "./assets/pages/Dashboard_new_invoice";
 
 const App = () => {
   return (
@@ -26,6 +21,10 @@ const App = () => {
         <Route path="/show_contact" element={<Show_contact />} />
         <Route path="*" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/dashboard/new-invoice"
+          element={<Dashboard_new_invoice />}
+        />
       </Routes>
     </BrowserRouter>
   );
