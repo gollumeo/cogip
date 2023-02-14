b4151b72ff747e68cb5e70fd4c5ef73fd5700f52
Benjamin Porsont <porsont.benjamin@gmail.com>
2023-02-14 16:59:25 +0100

    modif

---
+++ front/src/assets/component/Dash_Nav.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../scss/Dash_Nav.scss";
+
+const Dash_Nav = () => {
+  return (
+    <div className="dash_nav">
+      <div className="contact">
+        <img
+          className="dash_contact_img"
+          src="../../public/img/contact1.svg"
+          alt="contact"
+        />
+        <h1 className="dash_contact_name">
+          Henry <br /> George
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Dash_Nav;
