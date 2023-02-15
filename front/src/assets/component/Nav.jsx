45ef450a4aef458266b26897a04475340e98b2e0
MathildeCornelis <mathilde.cornelis26@gmail.com>
2023-02-15 16:50:59 +0100

    burger menu ok

--- front/src/assets/component/Nav.jsx
+++ front/src/assets/component/Nav.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import "/src/assets/scss/Nav.css";
-import BurgerMenu from '/public/img/burger_menu_icon.png';
 import { useState } from 'react';
 
 function Nav(){
@@ -18,35 +17,40 @@ return (
   <header>
     <h1>COGIP</h1>
     <nav className="all">
-      <button className="burger" onClick={() => setIsOpen(!isOpen)}>
-        <img src={BurgerMenu} alt="BurgerMenu" className='burger_menu'/>
-      </button>
-      <div className="nav_pages" style={{ display: displayValue, ...(window.matchMedia("(min-width: 1280px)").matches && {display: 'flex'}) }}>
-        <ul className='list_pages'>
-          <NavLink to ="/" className="nav_link">
-            <li className='page'>Home</li>
-          </NavLink>
-          <NavLink to="/invoices" className="nav_link">
-            <li className='page'>Invoices</li>
-          </NavLink>
-          <NavLink to="/companies" className="nav_link">
-            <li className='page'>Companies</li>
-          </NavLink>
-          <NavLink to="/contacts" className="nav_link">
-            <li className='page'>Contacts</li>
-          </NavLink>
-        </ul>
+      <a href='#' className="burger" onClick={() => setIsOpen(!isOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </a>
+      <div className="regroup_list">
+        <div className="nav_pages" style={{ display: displayValue, ...(window.matchMedia("(min-width: 1280px)").matches && {display: 'flex'}) }}>
+          <ul className='list_pages'>
+            <NavLink to ="/" className="nav_link">
+              <li className='page'>Home</li>
+            </NavLink>
+            <NavLink to="/invoices" className="nav_link">
+              <li className='page'>Invoices</li>
+            </NavLink>
+            <NavLink to="/companies" className="nav_link">
+              <li className='page'>Companies</li>
+            </NavLink>
+            <NavLink to="/contacts" className="nav_link">
+              <li className='page'>Contacts</li>
+            </NavLink>
+          </ul>
+        </div>
+        <div className="nav_log" style={{ display: displayValue, ...(window.matchMedia("(min-width: 1280px)").matches && {display: 'flex'}) }}>
+          <ul className='list_log'>
+            <NavLink to="/register.php" className="nav_link_2">
+              <li className='sign_up'>Sign up</li>
+            </NavLink>
+            <NavLink to="/login.php" className="nav_link">
+              <li className='login'>Login</li>
+            </NavLink>
+          </ul>
+        </div>
       </div>
-      <div className="nav_log" style={{ display: displayValue, ...(window.matchMedia("(min-width: 1280px)").matches && {display: 'flex'}) }}>
-        <ul className='list_log'>
-          <NavLink to="/register.php" className="nav_link_2">
-            <li className='sign_up'>Sign up</li>
-          </NavLink>
-          <NavLink to="/login.php" className="nav_link">
-            <li className='login'>Login</li>
-          </NavLink>
-        </ul>
-      </div>
+      
     </nav>
   </header>
 );
