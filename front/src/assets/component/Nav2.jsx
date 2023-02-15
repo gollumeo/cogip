957c8dc512938559b22b6e3ea4ecfa4080746275
MathildeCornelis <mathilde.cornelis26@gmail.com>
2023-02-15 16:50:59 +0100

    burger menu ok

--- front/src/assets/component/Nav2.jsx
+++ front/src/assets/component/Nav2.jsx
@@ -16,50 +16,38 @@ function Nav2() {
       <header>
         <h1>COGIP</h1>
         <nav className="all">
-          <button className="burger" onClick={() => setIsOpen(!isOpen)}>
-            <img src={BurgerMenu} alt="BurgerMenu" className="burger_menu" />
-          </button>
-          <div
-            className="nav_pages"
-            style={{
-              display: displayValue,
-              ...(window.matchMedia("(min-width: 1280px)").matches && {
-                display: "flex",
-              }),
-            }}
-          >
-            <ul className="list_pages">
-              <NavLink to="/" className="nav_link">
-                <li className="page">Home</li>
-              </NavLink>
-              <NavLink to="/invoices" className="nav_link">
-                <li className="page">Invoices</li>
-              </NavLink>
-              <NavLink to="/companies" className="nav_link">
-                <li className="page">Companies</li>
-              </NavLink>
-              <NavLink to="/contacts" className="nav_link">
-                <li className="page">Contacts</li>
-              </NavLink>
-            </ul>
-          </div>
-          <div
-            className="nav_log"
-            style={{
-              display: displayValue,
-              ...(window.matchMedia("(min-width: 1280px)").matches && {
-                display: "flex",
-              }),
-            }}
-          >
-            <ul className="list_log">
-              <NavLink to="*" className="nav_link_2">
-                <li className="sign_up">Sign up</li>
-              </NavLink>
-              <NavLink to="*" className="nav_link">
-                <li className="login">Login</li>
-              </NavLink>
-            </ul>
+          <a href='#' className="burger" onClick={() => setIsOpen(!isOpen)}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </a>
+          <div className="regroup_list">
+            <div className="nav_pages" style={{ display: displayValue, ...(window.matchMedia("(min-width: 1280px)").matches && {display: 'flex'}) }}>
+              <ul className='list_pages'>
+                <NavLink to ="/" className="nav_link">
+                  <li className='page'>Home</li>
+                </NavLink>
+                <NavLink to="/invoices" className="nav_link">
+                  <li className='page'>Invoices</li>
+                </NavLink>
+                <NavLink to="/companies" className="nav_link">
+                  <li className='page'>Companies</li>
+                </NavLink>
+                <NavLink to="/contacts" className="nav_link">
+                  <li className='page'>Contacts</li>
+                </NavLink>
+              </ul>
+            </div>
+            <div className="nav_log" style={{ display: displayValue, ...(window.matchMedia("(min-width: 1280px)").matches && {display: 'flex'}) }}>
+              <ul className='list_log'>
+                <NavLink to="/register.php" className="nav_link_2">
+                  <li className='sign_up'>Sign up</li>
+                </NavLink>
+                <NavLink to="/login.php" className="nav_link">
+                  <li className='login'>Login</li>
+                </NavLink>
+              </ul>
+            </div>
           </div>
         </nav>
       </header>
