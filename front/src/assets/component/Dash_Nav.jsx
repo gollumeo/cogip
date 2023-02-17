84fb24949b1ddbde13ae390451b410d5fa5d4c83
Benjamin Porsont <porsont.benjamin@gmail.com>
2023-02-17 09:06:16 +0100

    modif

--- front/src/assets/component/Dash_Nav.jsx
+++ front/src/assets/component/Dash_Nav.jsx
@@ -9,26 +9,45 @@ const Dash_Nav = () => {
   return (
     <div className="dash_nav">
       <div className="contact">
+        <div className="contact_top">
+          <img
+            className="dash_contact_img"
+            src="../../public/img/contact1.svg"
+            alt="contact"
+          />
+          <h1 className="dash_contact_name">
+            Henry <br /> George
+          </h1>
+        </div>
+        <div className="dash_navigation_list">
+          <ul>
+            <div className="li_nav">
+              <img src={dashboard} className="dashboard-img" />
+              <li>Dashboard</li>
+            </div>
+            <div className="li_nav">
+              <img src={invoices} className="invoices-img" />
+              <li>Invoices</li>
+            </div>
+            <div className="li_nav">
+              <img src={companies} className="companies-img" />
+              <li>Companies</li>
+            </div>
+            <div className="li_nav">
+              <img src={contact} className="contact-img" />
+              <li>Contacts</li>
+            </div>
+          </ul>
+        </div>
+      </div>
+
+      <div className="logout_nav">
         <img
-          className="dash_contact_img"
+          className="dash_contact_logout"
           src="../../public/img/contact1.svg"
           alt="contact"
         />
-        <h1 className="dash_contact_name">
-          Henry <br /> George
-        </h1>
-      </div>
-      <div className="dash_navigation_list">
-        <ul>
-          <img src={dashboard} className="dashboard-img" />
-          <li>Dashboard</li>
-          <img src={invoices} className="invoices-img" />
-          <li>Invoices</li>
-          <img src={companies} className="companies-img" />
-          <li>Companies</li>
-          <img src={contact} className="contact-img" />
-          <li>Contacts</li>
-        </ul>
+        <p>Logout</p>
       </div>
     </div>
   );
