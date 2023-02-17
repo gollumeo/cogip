7627a551a1c69149c4ccba754bfc3941afbd27dd
MathildeCornelis <mathilde.cornelis26@gmail.com>
2023-02-17 13:54:28 +0100

    login css

--- front/register.php
+++ front/register.php
@@ -8,7 +8,9 @@
 </head>
 
 <body class="body_form">
-  <a href="http://localhost:5173"><img src="./public/img/logo_COGIP.svg" alt="Logo Cogip" class="logo_cogip"></a>
+  <a href="http://localhost:5173" class="a_form">
+    <img src="./public/img/logo_COGIP.svg" alt="Logo Cogip" class="logo_cogip">
+  </a>
   <img src="./public/img/bulb.png" alt="bulb" class="bulb_php">
   <img src="./public/img/black notebook and green arm.png" alt="black notebook and green arm" class="arm_php">
   <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" class="form_content">
