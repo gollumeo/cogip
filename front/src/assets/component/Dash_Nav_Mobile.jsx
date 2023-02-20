8852f82c90ef6a6996153b952a707b0d818eef9f
Benjamin Porsont <porsont.benjamin@gmail.com>
2023-02-20 16:41:05 +0100

    modif

--- front/src/assets/component/Dash_Nav_Mobile.jsx
+++ front/src/assets/component/Dash_Nav_Mobile.jsx
@@ -1,27 +1,45 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 import "../scss/Dash_Nav_Mobile.scss";
 
 const Dash_Nav_Mobile = () => {
-  const [open, setOpen] = useState(false);
+  const menuHamburgerRef = useRef(null);
+  const navLinksRef = useRef(null);
+
+  const handleMenuClick = () => {
+    navLinksRef.current.classList.toggle("menu");
+  };
 
   return (
-    <div className="dash_nav_mobile">
-      <div className="contact_mobile">
-        <img
-          className="dash_contact_img_mobile"
-          src="../../public/img/contact1.svg"
-          alt="contact"
-        />
-        <h1 className="dash_contact_name_mobile">
-          Henry <br /> George
-        </h1>
+    <div>
+      <div className="dash_nav_mobile">
+        <div className="dash_nav_mobile_top">
+          <div className="contact_mobile">
+            <img
+              className="dash_contact_img_mobile"
+              src="../../public/img/contact1.svg"
+              alt="contact"
+            />
+            <h1 className="dash_contact_name_mobile">
+              Henry <br /> George
+            </h1>
+          </div>
+          <img
+            className="burger_menu"
+            src="../../public/img/burger_menu_icon.png"
+            alt=""
+            ref={menuHamburgerRef}
+            onClick={handleMenuClick}
+          />
+        </div>
+        <div className="nav_links" ref={navLinksRef}>
+          <ul>
+            <li>Dashboard</li>
+            <li>Invoices</li>
+            <li>Companies</li>
+            <li>Contacts</li>
+          </ul>
+        </div>
       </div>
-      <img
-        className="burger_menu"
-        src="../../public/img/burger_menu_icon.png"
-        alt=""
-        onClick={() => setOpen(!open)}
-      />
     </div>
   );
 };
